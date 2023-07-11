c5abe706a6a3b0ed31d2bb2210bdf99c3f62585f
Manibharathi <manibharath159@gmail.com>
2023-07-11 18:06:06 +0530

    Data Types Ranges added

---
+++ Example.java
@@ -0,0 +1,63 @@
+public class Example {
+    public static void main(String[] args)
+    {
+
+            /*
+             * Data Type Converstion
+             */
+            byte a = 12;
+            short b = a;
+            int c = 10980;
+            long d = c;
+
+            short e = 155;
+            byte f = (byte)e;
+
+            int g = 132;
+            short k = (short)g;    
+
+
+            System.out.println(e);
+            System.out.println(f);
+
+
+            byte age = 22;
+            System.out.println(age);
+            short myAge = 23;
+            System.out.println(myAge);
+            int my_age = 23;
+            System.out.println(my_age);
+            long MYAGE = 23;
+            System.out.println(MYAGE);
+
+            long mySalary = 100000000000L;
+
+            double my_salary = 10000.50;
+        
+           /*  System.out.println("Byte Range");
+            System.out.println(Byte.MIN_VALUE);
+            System.out.println(Byte.MAX_VALUE);
+
+            System.out.println("Short Range");
+            System.out.println(Short.MIN_VALUE);
+            System.out.println(Short.MAX_VALUE);
+
+            System.out.println("Integer Range");
+            System.out.println(Integer.MIN_VALUE);
+            System.out.println(Integer.MAX_VALUE);
+
+            System.out.println("Long Range");
+            System.out.println(Long.MIN_VALUE);
+            System.out.println(Long.MAX_VALUE);
+
+            System.out.println("Float Range");
+            System.out.println(Float.MIN_VALUE);
+            System.out.println(Float.MAX_VALUE);
+
+            System.out.println("Double Range");
+            System.out.println(Double.MIN_VALUE);
+            System.out.println(Double.MAX_VALUE);
+            */
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,35 @@
+class Student
+{
+    public static void main(String[] args)
+    {
+        int myAge = 25;
+        int mySalary = 1000000000;
+        System.out.println("Integer");
+        System.out.println("-----------------------------------");
+        System.out.println(myAge);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("-----------------------------------");
+        System.out.println("Long");
+        System.out.println("-----------------------------------");
+        long myToken = 10000000000L;
+        System.out.println(myToken);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println("-----------------------------------");
+        System.out.println("Float");
+        System.out.println("-----------------------------------");
+        float myPoints = 10.5f;
+        System.out.println(myPoints);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println("-----------------------------------");
+        System.out.println("Boolean");
+        System.out.println("-----------------------------------");
+        boolean isValid = true;
+        boolean isStatus = false;
+        System.out.println(isValid);
+        System.out.println(isStatus);
+
+    }
+}
