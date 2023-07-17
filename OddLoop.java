538352dda15f60a0d7d0c7aa30ff13387c452f79
Manibharathi <manibharath159@gmail.com>
2023-07-17 18:59:32 +0530

    Looping Added

---
+++ OddLoop.java
@@ -0,0 +1,9 @@
+public class OddLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        for(i=2; i<=10;i = i+2)
+        {
+            System.out.println(i);
+        }
+    }
+}
